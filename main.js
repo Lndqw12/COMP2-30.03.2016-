d1c37149a1dc4bdd6ef36dae033fd9a2725417d8
Lndqw12 <COMP222012016@mail.ru>
2016-03-30 17:48:39 +0300

    Comp2
    Comp2

---
+++ main.js
@@ -0,0 +1,25 @@
+﻿(function() {
+
+	var div = document.getElementsByClassName('classUL');
+		style = div[0].style;
+		style.color = "green";
+		style.background = "#948192";
+		div.className = "classUL";
+})();
+
+
+function change_font() {
+var par = document.getElementById('par');
+var color = 'white';
+ var c = setInterval ( function() {
+ for (i = 0; i < 1000; i++) {
+ 
+ if ( par.style.color == 'black') {
+ color = 'white'; 
+ } else if (par.style.color == 'white') {
+ color = 'black'};
+ }
+ par.style.color = color;
+ },1000);
+ }
+ change_font();
